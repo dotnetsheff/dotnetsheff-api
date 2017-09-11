71be9ba2201d54ebaf07fca211df4d081b1d2af2
kevbite <kev_bite@msn.com>
2017-09-11 12:54:55 +0100

    Adding a KeepAlive function to keep the app running every 5mins doing nothing.

---
+++ src/dotnetsheff.Api/KeepAlive/KeepAlive.cs
@@ -0,0 +1,14 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace dotnetsheff.Api.KeepAlive
+{
+    public static class KeepAlive
+    {
+        [FunctionName("KeepAlive")]
+        public static void Run([TimerTrigger("5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            // Do nothing...
+        }
+    }
+}
