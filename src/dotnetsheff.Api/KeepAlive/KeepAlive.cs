23cb02eb2462bbe15ce6d8ad195f577ab073f5db
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-10-04 10:31:08 +0100

    PR changes: Remove redundant .gitignore and Add KeepAlive function back.

---
+++ src/dotnetsheff.Api/KeepAlive/KeepAlive.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;	
+
+namespace dotnetsheff.Api.KeepAlive	
+{	
+    public static class KeepAlive	
+    {	
+        [FunctionName("KeepAlive")]	
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)	
+        {	
+            // Do nothing...	
+        }	
+    }	
+}
